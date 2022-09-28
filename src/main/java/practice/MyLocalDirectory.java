7a0538536f5d608b607bfabbd5dc4c989c780586
Pratik <impratikbhowmik@gmail.com>
2022-09-28 08:14:09 +0530

    Added practice code

--- src/main/java/practice/MyLocalDirectory.java
+++ src/main/java/practice/MyLocalDirectory.java
@@ -1,11 +1,8 @@
 package practice;
-
 public class MyLocalDirectory {
-
     public static void main(String[] args) {
-
-
         String filepath = System.getProperty("user.dir");
         System.out.println("Current working directory of this system is "+filepath);
+        System.out.println("Got the file path");
     }
 }
